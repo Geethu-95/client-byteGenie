e0ccbf7612bf7da20971da82a6b063e83d560c1d
Geethika S <geethu.s1995@gmail.com>
2023-12-17 09:57:28 +0530

    Updated files

--- README.md
+++ README.md
@@ -1,70 +1,21 @@
-# Getting Started with Create React App
+## UI FOR DATA SUMMARIZER
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+1. FUNCTIONALITIES
+Provision for providing .csv file containing document text fragments in the first column named 'text' and therby summarizing it. Option to select which text fragment to summarize.
 
-## Available Scripts
+Provision for entering data in textbox and summarizing it.
 
-In the project directory, you can run:
+2. API ENDPOINTS
 
-### `npm start`
+http://127.0.0.1:5000/summary_generate_partialDoc/${data} - For summarizing csv document text fragments.
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+http://127.0.0.1:5000/summary_generate/${url} - For summarizing text entered in text box.
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+3. CHALLENGES
 
-### `npm test`
+Styling the components
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+4. IMPROVEMENTS
 
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+Better UI design.
+Option to summarize entire documents.
--- src/App.js
+++ src/App.js
@@ -1,23 +1,162 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Papa from "papaparse";
+import axios from 'axios';
+import { Box, Button, Grid, Typography } from "@mui/material";
+import TextField from '@mui/material/TextField';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
 
 function App() {
+  // State to store parsed data
+  const [parsedData, setParsedData] = useState([{}]);
+
+  //State to store table Column name
+  const [tableRows, setTableRows] = useState([]);
+
+  //State to store the values
+  const [values, setValues] = useState();
+
+  const [data, setData] = useState(undefined);
+
+  const [url, setUrl] = useState();
+
+  const [csvFragmentSummarized, setCsvFragmentSummarized] = useState();
+
+  const [textSummarized, setTextSummarized] = useState();
+
+
+  const changeHandler = (event) => {
+    let rowsArray = [];
+    let valuesArray = [];
+    let optionsArray = [];
+    // Passing file data (event.target.files[0]) to parse using Papa.parse
+    Papa.parse(event.target.files[0], {
+      header: true,
+      skipEmptyLines: true,
+      complete: function (results) {
+        // let rowsArray = [];
+        // let valuesArray = [];
+        results.data.map((d) => {
+          rowsArray.push(Object.keys(d));
+          valuesArray.push(Object.values(d));
+          optionsArray.push(d.text);
+        });
+        parsedData.push(results.data);
+        setTableRows(rowsArray[0]);
+        // setValues(valuesArray);
+        setValues(optionsArray)
+
+
+      }
+
+    })
+    console.log(values)
+
+  }
+
+  const onOptionChangeHandler = (event) => {
+    setData(event.target.value);
+    console.log(
+      "User Selected Value - ",
+      event.target.value
+    );
+  };
+
+  const submitHandler = () => {
+    axios.get(`http://127.0.0.1:5000/summary_generate_partialDoc/${data}`).then((result) => {
+
+      console.log(result)
+      setCsvFragmentSummarized(result.data.summary)
+    }).catch((err) => {
+      console.log(err)
+    })
+
+  }
+  const onInputChangeHandler = (event) => {
+    setUrl(event.target.value);
+    console.log(
+      "Entered URL - ",
+      event.target.value
+    );
+  };
+
+  const submitUrlHandler = () => {
+
+    axios.get(`http://127.0.0.1:5000/summary_generate/${url}`).then((result) => {
+
+      console.log(result)
+      setTextSummarized(result.data.summary)
+    }).catch((err) => {
+      console.log(err)
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div align="center">
+      <Typography variant="h3" className="headingText">DATA SUMMARIZER</Typography>
+      <Grid container>
+
+        <Grid item xs={6}>
+          <label><b>Select csv file containing text fragments</b></label>
+          <input
+            type="file"
+            name="file"
+            onChange={changeHandler}
+            accept=".csv"
+            style={{ display: "block", margin: "10px auto" }}
+          />
+          <br />
+          <br />
+          <Select onChange={onOptionChangeHandler} value={data} className="selectComp">
+            <MenuItem>Choose text fragment to summarize</MenuItem >
+            {values && values.map((option, index) => {
+              return (
+                <MenuItem value={option} key={index}>
+                  {option}
+                </MenuItem >
+              );
+            })}
+          </Select>
+          <br></br>
+            <h5>You selected: </h5>
+            <Box className="BoxStyle">
+            {data}
+          </Box>
+          <br></br>
+          <Button variant="contained" onClick={submitHandler}>SUMMARIZE</Button>
+         
+        </Grid>
+
+        <Grid item xs={6}>
+          <TextField
+            className="textBox"
+            id="outlined-multiline-static"
+            label="Text to summarize"
+            multiline
+            rows={8}
+            // defaultValue="Text to summarize"
+            onChange={onInputChangeHandler}
+            value={url}
+          />
+          <br></br> <br></br>
+          <Button variant="contained" onClick={submitUrlHandler}>SUMMARIZE</Button>
+
+        </Grid>
+        <br></br>  <br></br>  <br></br>
+        <Grid item xs={12}>
+          <Typography variant="h4">SUMMARIZED TEXT</Typography>
+          <br></br>
+          <Typography variant="h7">
+            <Box className="BoxStyle">
+              {textSummarized ? textSummarized : csvFragmentSummarized}
+            </Box>
+
+          </Typography>
+       
+      </Grid>
+
+      </Grid>
+        <br></br><br></br><br></br>
     </div>
   );
 }
